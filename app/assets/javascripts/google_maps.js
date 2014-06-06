a670809b1c6a256e90f09362ad10102128b804e2
Proalab <alexandr.basan@gmail.com>
2014-06-06 13:02:58 +0300

    contacts geocode

--- app/assets/javascripts/google_maps.js
+++ app/assets/javascripts/google_maps.js
@@ -1,9 +1,18 @@
+var geocoder;
+var map;
 function initialize() {
+    geocoder = new google.maps.Geocoder();
+    var latlng = new google.maps.LatLng(50.450100, 30.523400);
     var mapOptions = {
         zoom: 8,
-        center: new google.maps.LatLng(50.450100, 30.523400)
+        center: latlng
     }
-    var map = new google.maps.Map(document.getElementById('map-canvas'),
+
+    // for geocode
+    //var map = new google.maps.Map(document.getElementById('map-canvas'),
+    //    mapOptions);
+    // without geocode
+    map = new google.maps.Map(document.getElementById('map-canvas'),
         mapOptions);
 
     setMarkers(map, beaches);
@@ -86,4 +95,36 @@ function setMarkers(map, locations) {
     }
 }
 
+
+function codeAddress() {
+    var address = document.getElementById('address').value;
+    var image = {
+        url: '/assets/bullet.png',
+        // This marker is 20 pixels wide by 32 pixels tall.
+        size: new google.maps.Size(28, 29),
+        // The origin for this image is 0,0.
+        origin: new google.maps.Point(0,0),
+        // The anchor for this image is the base of the flagpole at 0,32.
+        anchor: new google.maps.Point(0, 32)
+    };
+    geocoder.geocode( { 'address': address}, function(results, status) {
+        if (status == google.maps.GeocoderStatus.OK) {
+            map.setCenter(results[0].geometry.location);
+            map.setZoom(14);
+            var marker = new google.maps.Marker({
+                map: map,
+                position: results[0].geometry.location,
+                icon: image
+            });
+        } else {
+            alert('Geocode was not successful for the following reason: ' + status);
+        }
+    });
+}
+
 google.maps.event.addDomListener(window, 'load', initialize);
+
+
+google.maps.event.addDomListener(window, 'load', initialize);
+
+
